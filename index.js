15f5f5996b7b2daa0efcf06ec71d0b2b65bb053d
bguilas <bryanjguilas@gmail.com>
2021-02-23 19:42:13 -0800

    basic crud

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 const PORT = 5000 || process.env;
 const path = require('path')
 
@@ -16,11 +17,11 @@ db.once('open', () => {
 		console.log('Database connected');
 });
 
-
 const app = express()
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
 
 //ejs setup
 app.set('view engine', 'ejs');
@@ -52,7 +53,21 @@ app.get('/campgrounds/:id', async (req, res) => {
 
 //update route
 app.get('/campgrounds/:id/edit', async (req, res) => {
-		
+		const campground = await Campground.findById(req.params.id)
+		res.render('campgrounds/edit', {campground});
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+		const {id} = req.params;
+		const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+		res.redirect(`/campgrounds/${campground._id}`);
+})
+
+//delete
+app.delete('/campgrounds/:id', async (req, res) => {
+		const {id} = req.params;
+		 await Campground.findByIdAndDelete(id);
+		 res.redirect('/campgrounds');
 })
 
 app.listen(PORT, () => {
