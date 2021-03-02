81e8c88ec3868a7b06fee1bc699817aa0f09032b
bguilas <bryanjguilas@gmail.com>
2021-03-01 21:15:55 -0800

    middelware

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = 5000 || process.env;
 const path = require('path')
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const { campgroundSchema } = require('./schemas');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 		useNewUrlParser: true,
@@ -37,13 +38,25 @@ app.get('/campgrounds', catchAsync(async (req, res) => {
 		res.render('campgrounds/index', {campgrounds});
 }))
 
+//Middleware we created
+const validateCampground = (req, res, next) => {
+		const { error } = campgroundSchema.validate(req.body);
+		if(error) {
+				const msg = error.details.map(el => el.message).join(',')
+				throw new ExpressError(msg, 400 )
+		} else {
+				next();
+		}
+}
+
 //create
 app.get('/campgrounds/new', (req, res) => {
 		res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', catchAsync(async (req, res) => {
-		if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
+		// if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
 		const campground = new Campground(req.body.campground)
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`)
@@ -62,7 +75,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 		res.render('campgrounds/edit', {campground});
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id',validateCampground, catchAsync(async (req, res) => {
 		const {id} = req.params;
 		const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
 		res.redirect(`/campgrounds/${campground._id}`);
