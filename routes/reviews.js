03b2e8a58d628a7649adbceb2c191363cef956ff
bguilas <bryanjguilas@gmail.com>
2021-03-17 21:00:53 -0700

    stars

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,13 +3,11 @@ const router = express.Router({ mergeParams: true });
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 const Campground = require('../models/campground');
 const Review = require('../models/reviews');
+const reviews = require('../controllers/reviews');
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 
-//controllers
-const reviews = require('../controllers/reviews')
-
-router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview()))
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
 router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
