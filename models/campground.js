0cd8774d6ea35907d789a26eb9b55f6bd59f0056
bguilas <bryanjguilas@gmail.com>
2021-03-18 20:27:20 -0700

    added cloudinary

--- models/campground.js
+++ models/campground.js
@@ -1,15 +1,35 @@
 const mongoose = require('mongoose');
-const Review = require('./reviews');
+const Review = require('./reviews')
 const Schema = mongoose.Schema;
+
+
+// https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
+
+const ImageSchema = new Schema({
+		url: String,
+		filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+		return this.url.replace('/upload', '/upload/w_200');
+});
+
+const opts = { toJSON: { virtuals: true } };
 
 const CampgroundSchema = new Schema({
 		title: String,
-		images: [
-				{
-						url: String,
-						filename: String,
+		images: [ImageSchema],
+		geometry: {
+				type: {
+						type: String,
+						enum: ['Point'],
+						required: true
+				},
+				coordinates: {
+						type: [Number],
+						required: true
 				}
-		],
+		},
 		price: Number,
 		description: String,
 		location: String,
@@ -20,13 +40,22 @@ const CampgroundSchema = new Schema({
 		reviews: [
 				{
 						type: Schema.Types.ObjectId,
-						ref: 'Review',
+						ref: 'Review'
 				}
 		]
+}, opts);
+
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+		return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
 
+
+
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
-		if(doc) {
+		if (doc) {
 				await Review.deleteMany({
 						_id: {
 								$in: doc.reviews
